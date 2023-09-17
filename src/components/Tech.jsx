9838dabb69fe58d4af5b9ceb520c56afa115ad46
thnikas <8nikas@gmail.com>
2023-09-17 09:38:48 +0200

    first

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -5,13 +5,23 @@ import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 
 const Tech = () => {//the tech balls
+  const isSmallDevice = window.innerWidth <= 768; // Adjust the breakpoint as needed
+
   return (
     <div className='flex flex-row flex-wrap justify-center gap-10 mt-20'>
-      {technologies.map((technology) => (
+      {isSmallDevice? technologies.map((technology,index) => (
+        index<6?<div className='w-28 h-28' key={technology.name}>
+        <BallCanvas icon={technology.icon} />
+      </div>:null
+        
+      ))
+      : technologies.map((technology,index) => (
+        
         <div className='w-28 h-28' key={technology.name}>
           <BallCanvas icon={technology.icon} />
         </div>
       ))}
+     
     </div>
   );
 };
